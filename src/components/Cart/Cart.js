48245f62d98626692e33e28eef2c8039661265c4
Mahin <mahinyayaa@gmail.com>
2020-08-23 16:03:22 +0600

    shop page coding done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price
+        
+    }
+    let shipping = 0;
+    if(total > 200){
+        shipping = 0
+    }
+    else if(total > 100){
+        shipping = 6.99
+    }
+    else if(total > 0){
+        shipping = 12.99
+    }
+
+    const tax = (total / 10).toFixed(2)
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2)
+    return (
+        <div>
+            <h3>Order Summary</h3>
+            <p>Items ordered: {cart.length}</p>
+            <p>Product Price: ${total.toFixed(2)}</p>
+            <p>Shipping Cost: ${shipping}</p>
+            <p>Tax + Vat: ${tax}</p>
+            <p>Total Price: ${grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
